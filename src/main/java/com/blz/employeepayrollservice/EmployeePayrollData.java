7434e36d56817fd0ed57a3e56d0d50a58ccf352c
Pooja Sree <you@example.com>
2022-03-31 22:03:29 +0530

    added:UC1connectdatabaseretrivedatabase

---
+++ src/main/java/com/blz/employeepayrollservice/EmployeePayrollData.java
@@ -0,0 +1,27 @@
+package com.blz.employeepayrollservice;
+
+import java.time.LocalDate;
+
+public class EmployeePayrollData {
+
+    public int id;
+    public String name;
+    public double salary;
+    public LocalDate startDate;
+    public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.startDate = startDate;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", startDate=" + startDate +
+                '}';
+    }
+}
