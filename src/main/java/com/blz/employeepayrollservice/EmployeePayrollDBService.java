506e83c6ce459451f6a0a0cd602371e5b0c034fc
Pooja Sree <you@example.com>
2022-03-31 21:36:53 +0530

    added:welcome message

---
+++ src/main/java/com/blz/employeepayrollservice/EmployeePayrollDBService.java
@@ -0,0 +1,12 @@
+package com.blz.employeepayrollservice;
+
+import java.sql.*;
+
+public class EmployeePayrollDBService {
+
+
+    public static void main(String[] args) throws SQLException {
+        System.out.println("welcome to emplyeepayroll jdbc");
+
+    }
+    }
