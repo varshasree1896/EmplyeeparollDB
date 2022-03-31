bf977bcb1e6b1a296267b040a2eab2aaff3328ca
Pooja Sree <you@example.com>
2022-03-31 21:56:42 +0530

    added:UC1connectdatabaseretrivedatabase

--- src/main/java/com/blz/employeepayrollservice/EmployeePayrollDBService.java
+++ src/main/java/com/blz/employeepayrollservice/EmployeePayrollDBService.java
@@ -6,7 +6,31 @@ public class EmployeePayrollDBService {
 
 
     public static void main(String[] args) throws SQLException {
-        System.out.println("welcome to emplyeepayroll jdbc");
+        //Connect with database
+        String jdbcUrl="jdbc:mysql://localhost:3306/Payroll_services?useSSL=false";
+        String userName="root";
+        String password="root";
+        Connection con;
+        Statement statement ;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
+            con= DriverManager.getConnection(jdbcUrl,userName,password);
+            statement= con.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
+            while(resultSet.next()){
+                int  customerId = resultSet.getInt("id");
+                String customerName=resultSet.getString("name");
+                int salary=resultSet.getInt("salary");
+                System.out.print(customerId+" . ");
+                System.out.print(customerName+ " = " );
+                System.out.print(salary+" , ");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
     }
     }
